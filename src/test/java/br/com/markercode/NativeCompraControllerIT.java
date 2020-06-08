1be398a3e1042ef110f017eea17ccb2b1578d180
Gustavo Faria <gustavofaria@Mac-mini-de-Gustavo.local>
2020-06-08 15:35:42 -0300

    semTeste2

--- src/test/java/br/com/markercode/NativeCompraControllerIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.markercode;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeCompraControllerIT extends CompraControllerTest {
-
-    // Execute the same tests but in native mode.
-}
